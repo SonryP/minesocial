a119c47cab50fefdf0f7b51dc25ce1a97fb7ea09
Víctor Vásquez <sonry001@gmail.com>
2024-12-20 21:44:09 -0300

    Refactor activation page to improve error handling and code clarity

--- app/activation/page.tsx
+++ app/activation/page.tsx
@@ -1,37 +1,44 @@
-'use client'
-import { useSearchParams } from 'next/navigation'
-import RegistrationForm from '../components/RegistrationForm'
-import { getUser } from '../actions/activate'
-import { Suspense, useEffect, useState } from 'react'
+'use client';
+import { useSearchParams } from 'next/navigation';
+import RegistrationForm from '../components/RegistrationForm';
+import { getUser } from '../actions/activate';
+import { useEffect, useState } from 'react';
+
+export const dynamic = 'force-dynamic'; // Evita el prerenderizado estático.
 
 export default function Activation() {
   const [username, setUsername] = useState<string | null>(null);
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
+
   useEffect(() => {
     const fetchUsername = async () => {
-      const aid = searchParams.get('aid');
-      
-      if (aid) {
-        const name = await getUser(aid);
-        setUsername(name);
-        localStorage.setItem('pretoken', aid);
+      try {
+        const aid = searchParams.get('aid');
+        if (aid) {
+          const name = await getUser(aid);
+          setUsername(name || '');
+          localStorage.setItem('pretoken', aid);
+        }
+      } catch (error) {
+        console.error('Error fetching username:', error);
+        setUsername(''); // Manejo de fallback.
       }
     };
     fetchUsername();
-  }, []);
+  }, [searchParams]);
 
   return (
-   <Suspense>
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black bg-opacity-50">
-      { username == null ?  <h1 className="text-4xl text-white font-bold mb-8">Cargando...</h1>
-      :username == "" ? <h1 className="text-4xl text-white font-bold mb-8">Usuario ya activado!</h1>:
-      <>
-      <h1 className="text-4xl text-white font-bold mb-8">Bienvenido, {username}!</h1>
-      <RegistrationForm username={username} />
-    </>
-      }
+      {username === null ? (
+        <h1 className="text-4xl text-white font-bold mb-8">Cargando...</h1>
+      ) : username === '' ? (
+        <h1 className="text-4xl text-white font-bold mb-8">Usuario ya activado!</h1>
+      ) : (
+        <>
+          <h1 className="text-4xl text-white font-bold mb-8">Bienvenido, {username}!</h1>
+          <RegistrationForm username={username} />
+        </>
+      )}
     </main>
-    </Suspense>
-  )
+  );
 }
-
